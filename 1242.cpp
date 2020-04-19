213cba07d307ee3a8db0a02db7d0f500f8a2d24b
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2020-04-19 10:29:12 -0300

    Exercicios 19/04

--- 1242.cpp
+++ 1242.cpp
@@ -4,9 +4,30 @@
 *   1242 - Ácido Ribonucleico Alienígena
 */
 #include <iostream>
+#include <string>
+#include <stack>
 
 using namespace std;
 
 int main(){
+    string entrada;
 
+    while(getline(cin, entrada)){
+        stack<char> rnaa;
+        int cont=0;
+
+        for(int i=0;i<entrada.size();i++){
+            if(!rnaa.empty() && (entrada.at(i)=='C' && rnaa.top()=='F' ||
+                                 entrada.at(i)=='F' && rnaa.top()=='C' ||
+                                 entrada.at(i)=='S' && rnaa.top()=='B' ||
+                                 entrada.at(i)=='B' && rnaa.top()=='S')){
+                rnaa.pop();
+                cont++;
+            }
+            else 
+                rnaa.push(entrada.at(i));
+        }
+        cout << cont << endl;
+    }
+    return 0;
 }
--- 1523.cpp
+++ 1523.cpp
@@ -1,12 +1,58 @@
 /*  @toffanetto
 *   Gabriel Toffanetto França da Rocha
 *   Universidade Federal de Itajubá - Campus Itabira
-
+    1523 - Estacionamento Linear
 */
 #include <iostream>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
 int main(){
+    int n, k, c, s, last;
+    bool flag0;
 
+    while(true){
+
+        cin >> n >> k;
+        if(n==0 && k==0)
+            break;
+
+        flag0=true;
+        stack<int> patio;
+        vector<int> entrada (100001);
+        vector<int> saida (100001);   
+
+        for(int i=1;i<=n;i++){
+            cin >> c >> s;
+            if(s>last  || i==0){
+                last = s;
+            }
+            saida.at(s)=c;
+            entrada.at(c)=c;
+        }
+
+        for(int i=1;i<=last;i++){
+            if(saida.at(i)!=0){
+                if(!patio.empty() && patio.top()==saida.at(i))
+                    patio.pop();
+                    else{
+                        flag0=false;
+                        break;
+                    }
+            }
+            if(entrada.at(i)!=0){
+                if(patio.size()<k)
+                    patio.push(entrada.at(i));
+                    else{ 
+                        flag0=false;
+                        break;
+                    }
+            }
+        }
+
+        string s = (flag0) ? "Sim" : "Nao";
+        cout << s << endl;
+    }
 }
