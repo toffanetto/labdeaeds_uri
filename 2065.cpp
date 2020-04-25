a667fda372dad25fa90434875e02fae3e2dbdb13
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2020-04-25 13:14:41 -0300

    Atualização 25/04

--- 2065.cpp
+++ 2065.cpp
@@ -1,5 +1,35 @@
 /*  @toffanetto
 *   Gabriel Toffanetto França da Rocha
 *   Universidade Federal de Itajubá - Campus Itabira
-
+*   2065 - Fila do Supermercado
 */
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, m,c, identidade, livre, tempo;
+    priority_queue<pair<int,int>> fila; // <livre, tempo> 
+    
+    cin >> n >> m;
+
+    vector<int> v(n); // tempo de cada funcionario
+
+    for(int i=0;i<n;i++){
+        cin >> v.at(i);
+        fila.push(make_pair(0,-i));
+    }
+    for(int i=0; i<m;i++){
+        cin >> c;
+        livre=-fila.top().first; // tempo atual
+        identidade = -fila.top().second; // primeiro caixa livre
+        tempo = max(tempo, v.at(identidade)*c+livre);
+        fila.pop();
+        fila.push(make_pair(-(v.at(identidade)*c+livre),-identidade)); // tempo atual apos o atendimento, identidade do caixa
+    }
+
+    cout << tempo << endl;
+}
--- 2893.cpp
+++ 2893.cpp
@@ -1,5 +1,45 @@
 /*  @toffanetto
 *   Gabriel Toffanetto França da Rocha
 *   Universidade Federal de Itajubá - Campus Itabira
-
+*   2893 - Fibonac^{k}i
 */
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    int t, n, k;
+
+    cin >> t;
+
+    for(int i=0;i<t;i++){
+        cin >> k >> n;
+        queue<long int> sequencia;
+        long int soma=0;
+
+        if(n<k){
+            cout << n-1 << endl;
+            continue;
+        }
+
+        for(int j=0;j<n;j++){
+            if(j<k)
+                sequencia.push(j);
+                else{
+                    for(int l=0;l<k;l++){
+                        long int aux = sequencia.front();
+                        soma+=aux;
+                        sequencia.push(aux);
+                        sequencia.pop();
+                    }
+                    sequencia.push(soma%1000007);
+                    sequencia.pop();
+                }
+                soma=0;
+        }
+        cout << sequencia.back()%1000007 << endl;
+    }
+    cout  << endl;
+}
