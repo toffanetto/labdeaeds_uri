3b44a561154aec08e10c3ae0a1292a657c693ff5
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2020-04-25 13:47:52 -0300

    Atualizaçao 25/04

--- 1110.cpp
+++ 1110.cpp
@@ -1,5 +1,40 @@
 /*  @toffanetto
 *   Gabriel Toffanetto França da Rocha
 *   Universidade Federal de Itajubá - Campus Itabira
-
+    1110 - Jogando Cartas Fora
 */
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    int n;
+    queue<int> monte;
+    
+    while(true){
+        cin >> n;
+        if(n==0)
+            break;
+        
+        for(int i=1;i<=n;i++){
+            monte.push(i);
+        }
+        cout << "Discarded cards: ";
+        while(monte.size()>1){
+            int aux;
+            cout << monte.front();
+            monte.pop();
+            aux=monte.front();
+            monte.pop();
+            monte.push(aux);
+            if(monte.size()==1)
+            cout << endl;
+                else 
+                    cout << ", ";   
+        }
+        cout << "Remaining card: " << monte.front() << endl;
+        monte.pop();
+    }
+}
