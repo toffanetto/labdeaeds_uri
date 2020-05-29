778616caf237f13cdd839ab62497dbde0901efb3
Gabriel Toffanetto <toffanetto@users.noreply.github.com>
2020-05-29 12:07:31 -0300

    1633

--- 1633.cpp
+++ 1633.cpp
@@ -6,68 +6,38 @@
 
 #include <iostream>
 #include <queue>
+#include <functional>
 
 using namespace std;
 
-class Programa{
-public:
-    int tempo_chegou;
-    int tempo_executando;
+typedef pair<long long, long long> pll;
 
-    Programa(int tempo_chegou, int tempo_executando){
-        this->tempo_chegou = tempo_chegou;
-        this->tempo_executando = tempo_executando;
-    }
-    ~Programa(){};
-    bool operator<(const Programa&) const;
-};
-
-bool Programa::operator<(const Programa& p) const{
-    if(p.tempo_executando==tempo_executando)        // Não sei oq comparar aqui
-        return p.tempo_chegou<tempo_chegou;
-        else
-            return p.tempo_executando<tempo_executando; 
-}
 int main(){
     int n;
     while(cin >> n){
-
         uint64_t tempo=0;
         int t, c, clock=1, executando=0;
-        priority_queue<Programa> programas;
+        priority_queue<pll,  vector<pll>, greater<pll>> iniciar;
+        auto cmp = [](pll left, pll right) { return (left.second) > (left.first); };
+        priority_queue<pll,  vector<pll>, decltype(cmp) > executar(cmp);
 
         for(int i=0;i<n;i++){
             cin >> t >> c;
-            programas.push(Programa(t,c));
-            if(clock<programas.top().tempo_chegou){
-                clock++;
-                continue;
-            }
-            if(executando==0){
-                Programa task = programas.top();
-                programas.pop();
-                //cout << "TEMPO " << clock << " TEMPO PROGRAMA " << task.tempo_chegou << endl;
-                tempo+= (clock>task.tempo_chegou) ? (clock-task.tempo_chegou) : 0;
-                executando=task.tempo_executando;
-            }
-            clock++;
-            executando--;
+            iniciar.push(make_pair(t,c));
         }
 
-        while(!programas.empty()){
-            if(clock<programas.top().tempo_chegou){
-                clock++;
-                continue;
+        while(!iniciar.empty() || !executar.empty()){
+            while(!iniciar.empty() && (iniciar.top().first)<=clock){
+                executar.push(iniciar.top()); 
+                iniciar.pop();
             }
-            if(executando==0){
-                Programa task = programas.top();
-                programas.pop();
-                //cout << "TEMPO " << clock << " TEMPO PROGRAMA " << task.tempo_chegou << endl;
-                tempo+= (clock>task.tempo_chegou) ? (clock-task.tempo_chegou) : 0;
-                executando=task.tempo_executando;
-            }
-        clock++;
-        executando--;
+            if(!executar.empty()){
+                tempo+=(clock-executar.top().first);
+                clock+=executar.top().second;
+                executar.pop();
+            }   else{
+                    clock++;
+                }
         }
         cout << tempo << endl;
     }
