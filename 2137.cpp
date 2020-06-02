c6a89c25c044358ed2f85229e1a4192fe34743c7
Gabriel Toffanetto <toffanetto@users.noreply.github.com>
2020-06-02 10:26:16 -0300

    2137

---
+++ 2137.cpp
@@ -0,0 +1,28 @@
+/*  @toffanetto
+*   Gabriel Toffanetto França da Rocha
+*   Universidade Federal de Itajubá - Campus Itabira
+    2137 - A Biblioteca do Senhor Severino
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    while(cin>>n){
+        vector<string> catalago(n);
+        for(int i=0;i<n;i++){
+            cin >> catalago.at(i);
+        }
+        sort(catalago.begin(),catalago.end());
+        for(int i=0;i<n;i++){
+            cout << catalago.at(i) << endl;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
